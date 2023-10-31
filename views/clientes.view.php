503f58e74d92618d7eb562c4565e1d9b3f0bcbd8
AbrahamTO <100454390+AbrahamTO@users.noreply.github.com>
2023-10-30 21:14:59 -0500

    Update clientes.view.php

--- views/clientes.view.php
+++ views/clientes.view.php
@@ -62,6 +62,27 @@
                                         </td>
                                     </tr>
                                 <?php endwhile; ?>
+                                <script>
+                                    document.getElementById('searchProduct').addEventListener('keyup', function() {
+                                        var input, filter, table, tr, td, i, txtValue;
+                                        input = document.getElementById('searchProduct');
+                                        filter = input.value.toUpperCase();
+                                        table = document.getElementById('example2'); // ID de la tabla de productos
+                                        tr = table.getElementsByTagName('tr');
+
+                                        for (i = 0; i < tr.length; i++) {
+                                            td = tr[i].getElementsByTagName('td')[1]; // Columna del nombre del producto (ajusta el índice según tu estructura)
+                                            if (td) {
+                                                txtValue = td.textContent || td.innerText;
+                                                if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                                                    tr[i].style.display = '';
+                                                } else {
+                                                    tr[i].style.display = 'none';
+                                                }
+                                            }
+                                        }
+                                    });
+                                </script>
                             </tbody>
                         </table>
                     </div>
