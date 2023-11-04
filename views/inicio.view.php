0a09d48e1031442da7f1796c117fa44264504e1b
JharvinVela <148593332+JharvinVela@users.noreply.github.com>
2023-11-03 21:59:21 -0500

    Update inicio.view.php

--- views/inicio.view.php
+++ views/inicio.view.php
@@ -3,8 +3,7 @@
     <section class="content-header">
         <div class="container-fluid">
             <div class="row mb-2">
-                <h1><?php echo $mensaje; ?></h1>
+                <h1></h1>
             </div>
-        </div><!-- /.container-fluid -->
-    </section>
-</div>
+        </div>
+        <iframe title="clientes" width="1140" height="541.25" src="https://app.powerbi.com/reportEmbed?reportId=4790541d-93b2-407a-93b4-0d85756476eb&autoAuth=true&ctid=b4a40545-7779-4b38-aff7-1f1738f80840" frameborder="0" allowFullScreen="true"></iframe>
