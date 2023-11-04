d61d1b7d324fe453e20f3963feae065e71b2c257
JharvinVela <148593332+JharvinVela@users.noreply.github.com>
2023-11-03 22:01:30 -0500

    Update panel.php

--- panel.php
+++ panel.php
@@ -16,7 +16,7 @@ if (isset($_REQUEST['modulo']) && $_REQUEST['modulo'] == "cerrar") {
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>Aplicación</title>
+  <title>Tecnicell Pluss</title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <!-- Font Awesome -->
