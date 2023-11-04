273957209d4b75ddc05881a11dc62d2933c6d03e
JharvinVela <148593332+JharvinVela@users.noreply.github.com>
2023-11-04 11:53:34 -0500

    Update inicio.view.php

--- views/inicio.view.php
+++ views/inicio.view.php
@@ -6,4 +6,4 @@
                 <h1></h1>
             </div>
         </div>
-        <iframe title="clientes" width="1140" height="541.25" src="https://app.powerbi.com/reportEmbed?reportId=4790541d-93b2-407a-93b4-0d85756476eb&autoAuth=true&ctid=b4a40545-7779-4b38-aff7-1f1738f80840" frameborder="0" allowFullScreen="true"></iframe>
+<iframe title="clientes" width="1140" height="541.25" src="https://app.powerbi.com/reportEmbed?reportId=4790541d-93b2-407a-93b4-0d85756476eb&autoAuth=true&ctid=b4a40545-7779-4b38-aff7-1f1738f80840" frameborder="0" allowFullScreen="true"></iframe>
