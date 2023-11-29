1b4ec9ca726d6cad3fac728be2b2086e7d46c36c
AbrahamTO <100454390+AbrahamTO@users.noreply.github.com>
2023-11-29 11:22:07 -0500

    v3.0

--- clases/clsClientes.php
+++ clases/clsClientes.php
@@ -8,19 +8,21 @@ class Clientes
     public string $detalles;
     public string $numero;
     public string $fecha_registro;
+    public string $fecha_salida;
     public string $estado;
+    public string $precio;
 
     public function Registro($con)
     {
-        $query = "INSERT INTO cliente(nombres, imei, equipo, detalles, numero, fecha_registro, estado)
-        VALUES('$this->nombres', '$this->equipo', '$this->imei','$this->detalles', '$this->numero', '$this->fecha_registro', '$this->estado');";
+        $query = "INSERT INTO cliente(nombres, equipo , imei, detalles, numero, fecha_registro, fecha_salida, estado, precio)
+        VALUES('$this->nombres', '$this->equipo', '$this->imei','$this->detalles', '$this->numero', '$this->fecha_registro', '$this->fecha_salida', '$this->estado','$this->precio');";
 
         return mysqli_query($con, $query);
     }
 
     public function Consultar($con)
     {
-        $query = "SELECT * FROM cliente;";
+        $query = "SELECT * FROM cliente ORDER BY id_cliente DESC;";
         return mysqli_query($con, $query);
     }
 
@@ -35,7 +37,7 @@ class Clientes
     {
         $query = "UPDATE cliente
         SET nombres = '$this->nombres', equipo = '$this->equipo', imei = '$this->imei', detalles = '$this->detalles',
-        numero = '$this->numero', fecha_registro = '$this->fecha_registro', estado = '$this->estado' 
+        numero = '$this->numero', fecha_registro = '$this->fecha_registro', fecha_salida = '$this->fecha_salida', estado = '$this->estado', precio = '$this->precio' 
         WHERE id_cliente = '$this->id_cliente';";
         return mysqli_query($con, $query);
     }
@@ -43,6 +45,13 @@ class Clientes
     public function Eliminar($con)
     {
         $query = "DELETE FROM cliente WHERE id_cliente = '$this->id_cliente';";
-        return mysqli_query($con, $query);
+        $result = mysqli_query($con, $query);
+
+        $reorganizeQuery = "SET @counter = 0;
+                        UPDATE cliente SET id_cliente = @counter := @counter + 1;
+                        ALTER TABLE cliente AUTO_INCREMENT = 1;";
+        mysqli_multi_query($con, $reorganizeQuery);
+
+        return $result;
     }
 }
--- clases/clsProductos.php
+++ clases/clsProductos.php
@@ -5,18 +5,21 @@ class Productos
     public string $nombres;
     public string $cantidad;
     public string $categoria;
+    public string $precio;
+    public string $stock;
+    public string $devuelto;
 
     public function Registro($con)
     {
-        $query = "INSERT INTO productos(nombres, cantidad, categoria)
-        VALUES('$this->nombres', '$this->cantidad', '$this->categoria');";
+        $query = "INSERT INTO productos(nombres, cantidad, categoria, precio, stock, devuelto)
+        VALUES('$this->nombres', '$this->cantidad', '$this->categoria', '$this->precio', '$this->stock', '$this->devuelto');";
 
         return mysqli_query($con, $query);
     }
 
     public function Consultar($con)
     {
-        $query = "SELECT * FROM productos;";
+        $query = "SELECT * FROM productos ORDER BY id_productos DESC;";
         return mysqli_query($con, $query);
     }
 
@@ -30,7 +33,7 @@ class Productos
     public function Modificar($con)
     {
         $query = "UPDATE productos
-        SET nombres = '$this->nombres', cantidad = '$this->cantidad', categoria = '$this->categoria'
+        SET nombres = '$this->nombres', cantidad = '$this->cantidad', categoria = '$this->categoria', precio = '$this->precio', stock = '$this->stock', devuelto = '$this->devuelto'
           WHERE id_productos = '$this->id_productos';";
         return mysqli_query($con, $query);
     }
@@ -38,6 +41,13 @@ class Productos
     public function Eliminar($con)
     {
         $query = "DELETE FROM productos WHERE id_productos = '$this->id_productos';";
-        return mysqli_query($con, $query);
+        $result = mysqli_query($con, $query);
+
+        $reorganizeQuery = "SET @counter = 0;
+                        UPDATE productos SET id_productos = @counter := @counter + 1;
+                        ALTER TABLE productos AUTO_INCREMENT = 1;";
+        mysqli_multi_query($con, $reorganizeQuery);
+
+        return $result;
     }
 }
